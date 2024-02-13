9542dcaa4f54346a71e4fba5f3f0891a77a2caff
yasothaG <yasothag20@gmail.com>
2024-02-13 12:54:56 -0600

    i have changed the file

--- sample question.py
+++ sample question.py
@@ -1,6 +1,15 @@
-mark=input()
-if(mark>"35"):
+mark=int(input())
+if(mark>35):
     print("pass")
 else:
     print("fail")
         
+income= int(input("income:"))
+if(income>7000):
+    print("eligible for scholarship")
+else:
+    print("not eligible for scholarship")    
+
+
+a=10
+print(a/2)
