784abed9c59172fff85da3b2ba1fefa6d884f755
yasothaG <yasothag20@gmail.com>
2024-02-14 22:54:03 -0600

    updated

--- for loop .py
+++ for loop .py
@@ -58,4 +58,15 @@ print(a)
 sum=0
 for i in a:
     sum=sum+i
-print(sum)   
+print(sum)  
+
+#get 7 natural numbers and add the sum
+a=[]
+for i in range(7):
+    num=int(input("enter the number"+str(i+1)))
+    a.append(num)
+print(a)
+sum=0
+for i in a:
+    sum=sum+i
+print(sum)
--- samples.py
+++ samples.py
@@ -1,9 +1,5 @@
-a=[]
-for i in range(5):
-    num=int(input("enter num"+str(i+1)))
-    a.append(num)
-print(a)    
-sum=0
-for i in a:
-    sum=sum+i
-print(sum)    
+for i in range(1,6):
+    num=int(input("number is:" and "cube is"))
+print(i,i*i*i)
+
+
