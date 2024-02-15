798ba7f082fbf5d969a379f8db0afbf13191056d
yasothaG <139130244+yasothaG@users.noreply.github.com>
2024-02-15 01:42:52 -0600

    Update sample question.py

--- sample question.py
+++ sample question.py
@@ -5,7 +5,7 @@ if(mark>35):
 else:
     print("fail")
 
-  # if else method       
+# if else method       
 income= int(input("income:"))
 if(income>7000):
     print("eligible for scholarship")
@@ -35,8 +35,8 @@ if(a%2==0):
 else:
     print("odd")   
 
-    # get input  and say if input is lesser than 35 , then print poor student , if greater than 35 and less than 70 , then print average student
-    #if greater than 70 print good student 
+# get input  and say if input is lesser than 35 , then print poor student , if greater than 35 and less than 70 , then print average student
+#if greater than 70 print good student 
 score =int(input("out of 100:"))
 if(score<35):
     print("poor student")
@@ -45,7 +45,7 @@ if (score<35 and score<70):
 if(score>70):
     print("good student")   
 
-    #elif method
+#elif method
 score=int(input())    
 if(score<35):
     print("poor student")
@@ -54,8 +54,8 @@ elif(score>35 and score<70):
 else:
     print("good student")   
 
-    #Make a mini calculator, Get input for 2 numbers a and b  , Get input from user whether to add/sub/mul/div
-           #If user selects add then add the two number and Print the result.
+#Make a mini calculator, Get input for 2 numbers a and b  , Get input from user whether to add/sub/mul/div
+#If user selects add then add the two number and Print the result.
 a=int(input())
 b=int(input())
 operation =input ("add/sub/mul/div:")
@@ -72,7 +72,6 @@ else: print("Invalid operation")
 
 # This example is to set that we can add input inside the if condition  also 
 #/ get input for score, if score is greater than the 70 then only get the name, and department and finally print eligible or not 
-
 score=int(input())
 if(score>=70):
     name=input("enter your name:")
@@ -86,7 +85,6 @@ else:
    # Get input for salary and age.
    #If salary greater than or equal to 20000 or age less than or equal to 25, get input for required loan amount. If not print you are not eligible for loan.
    #If required loan amount is less than or equal to 50,000 print You are eligible for loan. If it is greater than 50,000 print maximum loan amount is 50000.
-
 salary=int(input("salary:"))
 age=int(input("age:"))
 if(salary>=20000 or age<=25):
@@ -99,8 +97,7 @@ else:
     print("not eligible for loan")
 
 
- #9. get input for five subjects marks. Add all of it And find average. If average mark is less than 35. Print "Additional class is required" else Print "you are good to go"
-
+#9. get input for five subjects marks. Add all of it And find average. If average mark is less than 35. Print "Additional class is required" else Print "you are good to go"
 Tamil=int(input())
 English=int(input())
 Maths=int(input())
